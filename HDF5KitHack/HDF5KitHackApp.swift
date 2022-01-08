9f11532b40805c7b2501f7a4777f1b39134e7fab
Geoffrey Lovelace <geoffrey4444@gmail.com>
2022-01-08 13:22:47 -0800

    Initial Commit

---
+++ HDF5KitHack/HDF5KitHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HDF5KitHackApp.swift
+//  HDF5KitHack
+//
+//  Created by Geoffrey Lovelace on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct HDF5KitHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
