fe150d5bbb80823b3b467b1ed8d28966a27dbe38
Geoffrey Lovelace <geoffrey4444@gmail.com>
2022-01-08 14:36:44 -0800

    Add minimal example

--- HDF5KitHack/ContentView.swift
+++ HDF5KitHack/ContentView.swift
@@ -6,16 +6,29 @@
 //
 
 import SwiftUI
+import HDF5Kit
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  @State private var result = ""
+  var body: some View {
+    Text(result).padding()
+    Button("Open H5 file") {
+      let path = "/Users/geoffrey/Desktop/GhKerrSchildReductions.h5"
+      guard let file = File.open(path, mode: .readWrite) else {
+        fatalError("Failed to open \(path)")
+      }
+      guard let norms = file.openDoubleDataset("/Norms.dat") else {
+        fatalError("Failed to open Norms.dat")
+      }
+      let norms_data = norms[0...0, 0...4]
+      result = String(norms_data[4])
+      print(norms_data)
+    }.padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
